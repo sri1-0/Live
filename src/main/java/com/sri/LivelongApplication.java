bc4ac652f83b3b2fa5f4c6ad90d149c9fe27399a
sriram <g.sriramkrishnan1000@gmail.com>
2025-08-26 09:31:02 +0530

    Initial commit

---
+++ src/main/java/com/sri/LivelongApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivelongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivelongApplication.class, args);
+	}
+
+}
